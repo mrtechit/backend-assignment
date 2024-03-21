6406de372bdd1a69ef5297959e46134afe9dfced
mrtechit <anirudhsinghforever@gmail.com>
2024-03-22 07:59:22 +1100

    add http server

--- txparser/main.go
+++ txparser/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
+// Port http server listen on.
+const portNum string = ":8181"
+
 func main() {
+
+	// Spinning up the server.
+	err := http.ListenAndServe(portNum, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
