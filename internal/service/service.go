0fee649913b7b3a038c24f018946628eeeacd353
mrtechit <anirudhsinghforever@gmail.com>
2024-03-22 08:08:24 +1100

    add type parser interface

---
+++ internal/service/service.go
@@ -0,0 +1,22 @@
+package service
+
+import "math/big"
+
+type Parser interface {
+	GetCurrentBlock() int
+	Subscribe(address string) bool
+	GetTransactions(address string) []Transaction
+}
+
+type Transaction struct {
+	ChainID     *big.Int `json:"chainId"`
+	BlockNumber *big.Int `json:"blockNumber"`
+	Hash        string   `json:"hash"`
+	Nonce       *big.Int `json:"nonce"`
+	From        string   `json:"from"`
+	To          string   `json:"to"`
+	Value       *big.Int `json:"value"`
+	Gas         *big.Int `json:"gas"`
+	GasPrice    *big.Int `json:"gasPrice"`
+	Input       string   `json:"input"`
+}
